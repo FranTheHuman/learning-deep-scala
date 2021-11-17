4a9bef231ac57ec2fb29ea661ae27199b01b4437
FranTheHuman <perrottafrancisco0@gmail.com>
2021-11-17 09:21:04 -0300

    Monoids - Semigroups

--- src/main/scala/scala_with_cats/Monoids_Semigroups.scala
+++ src/main/scala/scala_with_cats/Monoids_Semigroups.scala
@@ -9,21 +9,66 @@ object Monoids_Semigroups extends App {
     define the combine and empty operations and convince yourself that the monoid laws hold.
     Use the following definitions as a starting point:
    */
-  trait Semigroup[A] {
-    def combine(x: A, y: A): A
-  }
 
-  trait Monoid[A] extends Semigroup[A] {
-    def empty: A
-  }
+  object TheTruthAboutMonoids {
+    trait Semigroup[A] {
+      def combine(x: A, y: A): A
+    }
 
-  object Monoid {
-    def apply[A](implicit monoid: Monoid[A]): Monoid[A] = monoid
+    trait Monoid[A] extends Semigroup[A] {
+      def empty: A
+    }
+
+    object Monoid {
+      def apply[A](implicit monoid: Monoid[A]): Monoid[A] = monoid
+    }
+
+    implicit val booleanAndMonoid: Monoid[Boolean] =
+      new Monoid[Boolean] {
+        def combine(a: Boolean, b: Boolean) = a && b
+        def empty = true
+      }
+
+    implicit val booleanOrMonoid: Monoid[Boolean] =
+      new Monoid[Boolean] {
+        def combine(a: Boolean, b: Boolean) = a || b
+        def empty = false
+      }
+
+    implicit val booleanEitherMonoid: Monoid[Boolean] =
+      new Monoid[Boolean] {
+        def combine(a: Boolean, b: Boolean) = (a && !b) || (!a && b)
+        def empty = false
+      }
+
+    implicit val booleanXnorMonoid: Monoid[Boolean] =
+      new Monoid[Boolean] {
+        def combine(a: Boolean, b: Boolean) = (!a && b) && (a && !b)
+        def empty = true
+      }
   }
 
   // 2.4 Exercise: All Set for Monoids
 
   /* What monoids and semigroups are there for sets? */
+
+    object SetMonoids {
+      import TheTruthAboutMonoids._
+
+      implicit def setMonoid[A]: Monoid[Set[A]] =
+        new Monoid[Set[A]] {
+          override def combine(x: Set[A], y: Set[A]): Set[A] = x union y
+          override def empty: Set[A] = Set.empty[A]
+        }
+
+      val intSetMonoid = Monoid[Set[Int]]
+      val strSetMonoid = Monoid[Set[String]]
+
+      intSetMonoid.combine(Set(1, 2), Set(2, 3))
+      // res18: Set[Int] = Set(1, 2, 3)
+      strSetMonoid.combine(Set("A", "B"), Set("B", "C"))
+      // res19: Set[String] = Set("A", "B", "C")
+    }
 
   // 2.5.4 Exercise: Adding All The Things
 
@@ -33,14 +78,53 @@ object Monoids_Semigroups extends App {
     In a tragic accident this code is deleted! Rewrite the method and save the day!
    */
 
-  /*
-    Well done! SuperAdder’s market share continues to grow, and now there is demand for additional functionality.
-    People now want to add List[Option[Int]]. Change add so this is possible.
-    The SuperAdder code base is of the highest quality, so make sure there is no code duplication!
-   */
+    object SaveTheDay {
 
-  // SuperAdder is entering the POS (point-of-sale, not the other POS) market. Now we want to add up Orders:
-  case class Order(totalCost: Double, quantity: Double)
+      import cats.Monoid
+      import cats.instances.int._
+      import cats.syntax.semigroup._ // for |+| = combine
 
-  // We need to release this code really soon so we can’t make any modifications to add. Make it so!
+      def add_easy(items: List[Int]): Int =
+        items.sum // items.foldLeft(0)(_ + _)
+
+      def add_monoid(items: List[Int]): Int =
+        items.foldLeft(Monoid[Int].empty)(_ |+| _)
+
+      /*
+        Well done! SuperAdder’s market share continues to grow, and now there is demand for additional functionality.
+        People now want to add List[Option[Int]]. Change add so this is possible.
+        The SuperAdder code base is of the highest quality, so make sure there is no code duplication!
+      */
+
+      def superAdd[A](items: List[A])(implicit monoid: Monoid[A]): A =
+        items.foldLeft(monoid.empty)(_ |+| _) // (acc, it) => monoid.combine(acc, it)
+
+      def superAdd_2[A: Monoid](items: List[A]): A =
+        items.foldLeft(Monoid[A].empty)(_ |+| _)
+
+
+      superAdd_2(List(1, 2, 3)) // res10: Int = 6
+      superAdd_2(List(Some(1), None, Some(2), None, Some(3))) // res11: Option[Int] = Some(6)
+
+
+      // SuperAdder is entering the POS (point-of-sale, not the other POS) market. Now we want to add up Orders:
+
+      case class Order(totalCost: Double, quantity: Double)
+
+      // We need to release this code really soon so we can’t make any modifications to add. Make it so!
+
+      implicit val orderMonoid: Monoid[Order] = new Monoid[Order] {
+        override def empty: Order = Order(0, 0)
+
+        override def combine(x: Order, y: Order): Order =
+          Order(
+            totalCost = x.totalCost + y.totalCost,
+            quantity = x.quantity + y.quantity
+          )
+      }
+
+    }
+
+
+
 }
